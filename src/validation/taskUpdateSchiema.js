c72afed03c0a3e4fc54a8f6d43fc3754d0e9c2ad
artem-ilchuk <pmumart@gmail.com>
2025-12-23 10:31:09 +0200

    corrected taskvalidation

--- src/validation/taskUpdateSchiema.js
+++ src/validation/taskUpdateSchiema.js
@@ -1,19 +1,20 @@
 import Joi from 'joi';
 
 export const taskSchema = Joi.object({
-  title: Joi.string().min(1).max(100).required(),
-  description: Joi.string().allow('', null),
-  status: Joi.string().min(1).max(50).required(),
-  deadline: Joi.date().allow(null),
-  priority: Joi.string().valid('low', 'medium', 'high').optional(),
+  title: Joi.string().required(),
+  description: Joi.string().allow(''),
+  // Валидируем projectId как строку (ObjectId)
   projectId: Joi.string().required(),
+  status: Joi.string()
+    .valid('TO DO', 'IN PROGRESS', 'ON REVIEW', 'DONE')
+    .default('TO DO'),
+  deadline: Joi.date().allow(null),
 });
 
 export const taskUpdateSchema = Joi.object({
-  title: Joi.string().min(1).max(100),
-  description: Joi.string().allow('', null),
-  status: Joi.string().min(1).max(50).required(),
+  title: Joi.string(),
+  description: Joi.string().allow(''),
+  status: Joi.string().valid('TO DO', 'IN PROGRESS', 'ON REVIEW', 'DONE'),
   deadline: Joi.date().allow(null),
-  priority: Joi.string().valid('low', 'medium', 'high'),
-  projectId: Joi.string(),
-}).min(1);
+  order: Joi.number(),
+});
