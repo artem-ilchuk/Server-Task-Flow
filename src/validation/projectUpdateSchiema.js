ba8e8142703a4e59a83fa484a92edca4dee4fb83
artem-ilchuk <pmumart@gmail.com>
2025-12-24 23:20:13 +0200

    feat --adde ownerId

--- src/validation/projectUpdateSchiema.js
+++ src/validation/projectUpdateSchiema.js
@@ -2,10 +2,13 @@ import Joi from 'joi';
 
 export const projectSchema = Joi.object({
   title: Joi.string().min(1).max(100).required(),
-  description: Joi.string().allow('', null),
-  deadline: Joi.date().allow(null),
-  img: Joi.string().allow(null, ''),
-  status: Joi.string().allow(null, ''),
+  description: Joi.string().allow('', null).default(''),
+  deadline: Joi.date().allow(null).default(null),
+  img: Joi.string().allow(null, '').default(null),
+  ownerId: Joi.string()
+    .regex(/^[0-9a-fA-F]{24}$/)
+    .required()
+    .messages({ 'string.pattern.base': 'Invalid Owner ID format' }),
 });
 
 export const projectUpdateSchema = Joi.object({
